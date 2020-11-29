05b4b6cf4153077556c4d973e94883c4df92aca1
anaestheramaral <anaestheramaral@gmail.com>
2020-11-29 19:17:12 +0100

    chore: completed challenge

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,13 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // TODO
+
+// export const formatDat = new Intl.DateTimeFormat('pt-BR', {
+//   year: 'numeric',
+//   month: 'long',
+//   date: '2-digit',
+// }).format;
 
 export default formatValue;
